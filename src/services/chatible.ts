ac27f1b999a8dedc3512cf0ce4385cc9e278c04b
juno <cuongjunocn1@gmail.com>
2021-03-02 00:42:15 +0700

    fix

--- src/services/chatible.ts
+++ src/services/chatible.ts
@@ -271,7 +271,7 @@ const processEvent = async (event: WebhookMessagingEvent): Promise<void> => {
   } else if (!waitState && sender2 !== null) {
     // in chat room
     if (command === lang.KEYWORD_ISEND) {
-      await fb.sendTextButtons(sender, 'Bạn có chắc muốn kết thúc cuộc trò chuyện?', true, false, false, false, true, false);
+      await fb.sendTextButtons(sender, 'Bạn có chắc muốn kết thúc cuộc trò chuyện?', false, false, false, false, true, false);
     }
     else if (command === lang.KEYWORD_END) {
       await processEndChat(sender, sender2);
