8a1fdd698b64abaef4be16d93e64fe78e367e39f
juno <cuongjunocn1@gmail.com>
2021-03-25 14:33:00 +0700

    //

--- src/api/routes/admin.ts
+++ src/api/routes/admin.ts
@@ -19,7 +19,7 @@ router.post('/connect', async (req, res) => {
   let ret: AdminReplyProps = { success: false, error: true };
   try {
     const ret = await Admin.forceMatch(data.id1, data.id2, GenderEnum.FEMALE, GenderEnum.MALE)
-    res.send('success')
+    res.send('success' + req.body)
   } catch (error) {
     console.log(error)
     res.send('fail')
